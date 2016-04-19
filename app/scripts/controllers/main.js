19c53dab4b82de3540407240240c1c51e10491b6
Curtis Lacy <curtis.lacy@gmail.com>
2016-04-18 22:06:10 -0400

    Simple form and controller to take search terms.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the flickrSearchApp
  */
 angular.module('flickrSearchApp')
+
   .controller('MainCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  })
+
+  // CML: If this got any larger, we'd put each controller in its own file, but we'll
+  // leave it all together for now.
+  .controller('SearchFormCtrl', function( $scope ) {
+    $scope.doSearch = function() {
+      console.log( '* Search: ' + $scope.keyword );
+    };
   });
